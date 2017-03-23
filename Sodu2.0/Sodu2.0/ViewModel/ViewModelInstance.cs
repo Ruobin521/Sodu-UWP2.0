d0250407aa505e25fd4e7afdc10e5c941f86d734
DESKTOP-7IMSHJH\ruobi <83250112@qq.com>
2017-03-23 10:16:54 +0800

    初始化

---
+++ Sodu2.0/Sodu2.0/ViewModel/ViewModelInstance.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+
+namespace Sodu.ViewModel
+{
+    public class ViewModelInstance : ViewModelBase
+    {
+
+        private static readonly object SynObject = new object();
+
+        private static ViewModelInstance _instance;
+        public static ViewModelInstance Instance
+        {
+            get
+            {
+                lock (SynObject)
+                {
+                    return _instance ?? (_instance = new ViewModelInstance());
+                }
+            }
+        }
+
+        private MainViewModel _main;
+        public MainViewModel Main
+        {
+            get { return _main ?? (_main = new MainViewModel()); }
+            set
+            {
+                Set(ref _main, value);
+            }
+        }
+    }
+}
